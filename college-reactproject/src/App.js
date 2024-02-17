a5c136795b47c1024c828d1dd50aa8a058d97d86
Nikhilka4 <106381060+Nikhilka4@users.noreply.github.com>
2024-02-17 21:38:40 +0530

    g

--- college-reactproject/src/App.js
+++ college-reactproject/src/App.js
@@ -1,19 +1,19 @@
 import "./index.css";
-import HomePage from "./pages/HomePage";
+// import HomePage from "./pages/HomePage";
 // import LoginPage from "./pages/LoginPage";
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 // import LoginPage from "./pages/LoginPage";
 // import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import DashBoard from "./pages/DashBoard";
+import DashBoard from "./pages/DashBoard";
 // import UploadPage from "./pages/UploadPage";
 
         
 function App() {
   return (
     <>
-      <HomePage />
+      <DashBoard />
     </>
   );
 }
