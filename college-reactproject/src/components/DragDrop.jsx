073360fbb4f89b4ddc3205d3185f958eed70c930
Nikhilka4 <106381060+Nikhilka4@users.noreply.github.com>
2024-02-18 08:28:37 +0530

    b

--- college-reactproject/src/components/DragDrop.jsx
+++ college-reactproject/src/components/DragDrop.jsx
@@ -1,15 +1,55 @@
+// import "./DragDrop.css";
+
+
+// import React from 'react';
+// import { FileUpload } from 'primereact/fileupload';
+
+// export default function DragDrop() {
+        
+//     return (
+//         <div className="card Upload">
+//             <FileUpload name="demo[]" url={'/api/upload'} multiple accept="image/*" maxFileSize={1000000} emptyTemplate={<p className="m-0">Drag and drop files to here to upload.</p>} />
+//         </div>
+//     )
+// }
+import React, { useRef, useEffect, useState } from 'react';
+import { Toast } from 'primereact/toast';
+import { FileUpload } from 'primereact/fileupload';
 import "./DragDrop.css";
 
+export default function DragDrop() {
+    const toast = useRef(null);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-import React from 'react';
-import { FileUpload } from 'primereact/fileupload';
+    const onUpload = () => {
+        toast.current.show({ severity: 'info', summary: 'Success', detail: 'File Uploaded' });
+    };
 
-export default function AdvanceDemo() {
-        
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
-        <div className="card Upload">
-            <FileUpload name="demo[]" url={'/api/upload'} multiple accept="image/*" maxFileSize={1000000} emptyTemplate={<p className="m-0">Drag and drop files to here to upload.</p>} />
+        <div className="card">
+            {windowWidth <= 900 ? (
+                <div className="flex justify-content-center">
+                    <Toast ref={toast}></Toast>
+                    <FileUpload mode="basic" name="demo[]" url="/api/upload" accept="image/*" maxFileSize={1000000} onUpload={onUpload} />
+                </div>
+            ) : (
+                <div className="Upload">
+                    <FileUpload name="demo[]" url={'/api/upload'} multiple accept="image/*" maxFileSize={1000000} emptyTemplate={<p className="m-0">Drag and drop files to here to upload.</p>} />
+                </div>
+            )}
         </div>
-    )
+    );
 }
-      
+     
